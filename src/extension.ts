af2f1034afe3a791eca6bed922a6dbe62af463ca
BOCOVO <bocovojuste@gmail.com>
2022-12-29 00:59:25 +0100

    feat: display webview content

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,71 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "chat-gpt-vscode-extension" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chat-gpt-vscode-extension" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "chat-gpt-vscode-extension.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from chat-gpt-vscode-extension!"
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chat-gpt-vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from chat-gpt-vscode-extension!');
-	});
+  const webviewProvider = new GPTViewProvider(context.extensionUri);
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      GPTViewProvider.viewType,
+      webviewProvider,
+      {
+        webviewOptions: { retainContextWhenHidden: true },
+      }
+    )
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+class GPTViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "chat-gpt.webview";
+
+  // In the constructor, we store the URI of the extension
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.html = /*html*/ `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+		</head>
+		<body>
+			<h1>Content here</h1>
+		</body>
+		</html>
+		`;
+  }
 }
 
 // This method is called when your extension is deactivated
